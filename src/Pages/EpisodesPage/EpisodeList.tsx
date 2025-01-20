2e52f9ac4ac0b5fd0aa959c45222bd83156ccf5a
muhammadsaefulr <saepull.id@gmail.com>
2025-01-20 08:49:42 +0700

    fix episodelist

--- src/Pages/EpisodesPage/EpisodeList.tsx
+++ src/Pages/EpisodesPage/EpisodeList.tsx
@@ -15,7 +15,10 @@ const EpisodeList: React.FC<PageProps> = ({ dataEps }) => {
           <div className="grid gap-x-4">
             {dataEps?.slice().reverse().map((data: any, idx: number) => {
               return (
-                <Link key={idx} to={`/play/${data?.vidLinks.replace("https://otakudesu.cloud/", "")}`} className="pt-4">
+                <Link key={idx} to={`/play/episode/${data?.vidLinks.replace(data?.vidLinks.includes("https://otakudesu.cloud/lengkap")
+                  ? "https://otakudesu.cloud/lengkap/"
+                  : "https://otakudesu.cloud/",
+                "")}`} className="pt-4">
                   <button className="bg-gray-800 rounded-lg w-full p-3 flex justify-between transition delay-75 ease-in text-white hover:bg-gray-500">
                     <p>{data?.title}</p>
                     <PlaySquareIcon className="lg:visible md: hidden" />
